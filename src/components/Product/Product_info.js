0185d76bee712bb3f4c42988c76f092e2efa0d69
AB-Siddique278 <absiddique1060@gmail.com>
2022-03-26 01:11:30 +0600

    add name price and button element

--- src/components/Product/Product_info.js
+++ src/components/Product/Product_info.js
@@ -4,11 +4,24 @@ import './Product_info.css'
 const Product_info = (props) => {
     console.log(props.product)
 
+    const handleaddtocard = ()=>{
+            
+    }
+
     const {id, name, price, picture}= props.product
     return (
         <div className='product'>
             <img src={picture} alt="" />
-            
+            <h3>
+                Name: {name}
+            </h3>
+            <h4>
+                Price:$ {price}
+            </h4>
+            <button onClick={handleaddtocard} className='cart-button'>
+                <p>Add to Cart</p>
+            </button>
+
         </div>
     );
 };
